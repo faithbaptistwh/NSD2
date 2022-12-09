cd828e63f5fb6a82257f6bad0c0955b0277bfcbb
Brandon.DeVore <Brandon.DeVore@publix.com>
2022-12-08 21:44:02 -0500

    added link

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -42,7 +42,7 @@ class MainHero extends React.Component {
                       <Button
                         className='btn-white btn-icon mb-3 mb-sm-0'
                         color='default'
-                        href='/next-step-discipleship'
+                        href='/nsd'
                         size='lg'
                       >
                         <span className='btn-inner--icon mr-1'>
